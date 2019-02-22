03b95a1d2313998be14d660b314d41ef0d7152df
Austin Moran <a.moran3257@gmail.com>
2019-02-22 15:50:10 -0700

    filled out Immunities seed

--- seeds/04_Resistances.js
+++ seeds/04_Resistances.js
@@ -6,124 +6,124 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('Resistances').insert([
         // Iudex Gundyr
-        {id: 1, colName: 8},
-        {id: 1, colName: 9},
+        {boss_id: 1, damage_id: 8},
+        {boss_id: 1, damage_id: 9},
 
         // Vordt
-        {id: 2, colName: 3},
-        {id: 2, colName: 4},
+        {boss_id: 2, damage_id: 3},
+        {boss_id: 2, damage_id: 4},
 
         // Greatwood
 
         // Crystal Sage
-        {id: 4, colName: 5},
+        {boss_id: 4, damage_id: 5},
 
         // Abyss Watchers
-        {id: 5, colName: 8},
-        {id: 5, colName: 9},
+        {boss_id: 5, damage_id: 8},
+        {boss_id: 5, damage_id: 9},
 
         // Deacons
-        {id: 6, colName: 5},
-        {id: 6, colName: 8},
-        {id: 6, colName: 9},
+        {boss_id: 6, damage_id: 5},
+        {boss_id: 6, damage_id: 8},
+        {boss_id: 6, damage_id: 9},
 
         // Wolnir
-        {id: 7, colName: 8},
+        {boss_id: 7, damage_id: 8},
 
         // Demon King
-        {id: 8, colName: 6},
+        {boss_id: 8, damage_id: 6},
 
         // Pontiff
-        {id: 9, colName: 9},
-        {id: 9, colName: 10},
-        {id: 9, colName: 11},
-        {id: 9, colName: 12},
+        {boss_id: 9, damage_id: 9},
+        {boss_id: 9, damage_id: 10},
+        {boss_id: 9, damage_id: 11},
+        {boss_id: 9, damage_id: 12},
 
         // Yhorm
-        {id: 10, colName: 1},
-        {id: 10, colName: 2},
-        {id: 10, colName: 3},
-        {id: 10, colName: 4},
-        {id: 10, colName: 5},
-        {id: 10, colName: 8},
+        {boss_id: 10, damage_id: 1},
+        {boss_id: 10, damage_id: 2},
+        {boss_id: 10, damage_id: 3},
+        {boss_id: 10, damage_id: 4},
+        {boss_id: 10, damage_id: 5},
+        {boss_id: 10, damage_id: 8},
 
         // Aldrich
-        {id: 11, colName: 5},
-        {id: 11, colName: 8},
+        {boss_id: 11, damage_id: 5},
+        {boss_id: 11, damage_id: 8},
 
         // Dancer
-        {id: 12, colName: 9},
+        {boss_id: 12, damage_id: 9},
 
         // Dragonslayer Armor
-        {id: 13, colName: 7},
-        {id: 13, colName: 8},
+        {boss_id: 13, damage_id: 7},
+        {boss_id: 13, damage_id: 8},
 
         // Oceiros
-        {id: 14, colName: 5},
-        {id: 14, colName: 9},
+        {boss_id: 14, damage_id: 5},
+        {boss_id: 14, damage_id: 9},
 
         // Champion Gundyr
-        {id: 15, colName: 9},
-        {id: 15, colName: 10},
-        {id: 15, colName: 11},
+        {boss_id: 15, damage_id: 9},
+        {boss_id: 15, damage_id: 10},
+        {boss_id: 15, damage_id: 11},
 
         // Lorian
-        {id: 16, colName: 8},
+        {boss_id: 16, damage_id: 8},
 
         // Lothric
-        {id: 17, colName: 8},
+        {boss_id: 17, damage_id: 8},
 
         // Wyvern
-        {id: 18, colName: 9},
-        {id: 18, colName: 10},
-        {id: 18, colName: 11},
+        {boss_id: 18, damage_id: 9},
+        {boss_id: 18, damage_id: 10},
+        {boss_id: 18, damage_id: 11},
 
         // King of the Storm (dragon)
-        {id: 19, colName: 6},
-        {id: 19, colName: 9},
-        {id: 19, colName: 10},
-        {id: 19, colName: 11},
+        {boss_id: 19, damage_id: 6},
+        {boss_id: 19, damage_id: 9},
+        {boss_id: 19, damage_id: 10},
+        {boss_id: 19, damage_id: 11},
 
         // Nameless King
-        {id: 20, colName: 7},
-        {id: 20, colName: 9},
-        {id: 20, colName: 12},
+        {boss_id: 20, damage_id: 7},
+        {boss_id: 20, damage_id: 9},
+        {boss_id: 20, damage_id: 12},
 
         // Soul of Cinder
-        {id: 21, colName: 6},
-        {id: 21, colName: 9},
+        {boss_id: 21, damage_id: 6},
+        {boss_id: 21, damage_id: 9},
 
         // Champion's Gravetender
-        {id: 22, colName: 10},
-        {id: 22, colName: 11},
-        {id: 22, colName: 12},
+        {boss_id: 22, damage_id: 10},
+        {boss_id: 22, damage_id: 11},
+        {boss_id: 22, damage_id: 12},
 
         // Sister Friede
-        {id: 23, colName: 10},
-        {id: 23, colName: 11},
+        {boss_id: 23, damage_id: 10},
+        {boss_id: 23, damage_id: 11},
 
         // Demon Prince
-        {id: 24, colName: 1},
-        {id: 24, colName: 2},
-        {id: 24, colName: 4},
-        {id: 24, colName: 5},
-        {id: 24, colName: 6},
-        {id: 24, colName: 7},
+        {boss_id: 24, damage_id: 1},
+        {boss_id: 24, damage_id: 2},
+        {boss_id: 24, damage_id: 4},
+        {boss_id: 24, damage_id: 5},
+        {boss_id: 24, damage_id: 6},
+        {boss_id: 24, damage_id: 7},
 
         // Halflight
-        {id: 25, colName: 9},
-        {id: 25, colName: 10},
-        {id: 25, colName: 11},
+        {boss_id: 25, damage_id: 9},
+        {boss_id: 25, damage_id: 10},
+        {boss_id: 25, damage_id: 11},
 
         // Midir
-        {id: 26, colName: 5},
-        {id: 26, colName: 6},
-        {id: 26, colName: 8},
+        {boss_id: 26, damage_id: 5},
+        {boss_id: 26, damage_id: 6},
+        {boss_id: 26, damage_id: 8},
 
         // Gael
-        {id: 27, colName: 3},
-        {id: 27, colName: 8},
-        {id: 27, colName: 9},
+        {boss_id: 27, damage_id: 3},
+        {boss_id: 27, damage_id: 8},
+        {boss_id: 27, damage_id: 9}
       ]);
     });
 };
--- seeds/05_Immunities.js
+++ seeds/05_Immunities.js
@@ -1,13 +1,110 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('Immunities').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('Immunities').insert([
+        // Iudex Gundyr
+        {boss_id: 1, damage_id: 10},
+        {boss_id: 1, damage_id: 11},
+
+        // Vordt
+        {boss_id: 2, damage_id: 9},
+        {boss_id: 2, damage_id: 10},
+        {boss_id: 2, damage_id: 11},
+        {boss_id: 2, damage_id: 12},
+
+        // Greatwood
+        {boss_id: 3, damage_id: 9},
+        {boss_id: 3, damage_id: 10},
+        {boss_id: 3, damage_id: 11},
+        {boss_id: 3, damage_id: 12},
+
+        // Crystal Sage
+
+        // Abyss Watchers
+        {boss_id: 5, damage_id: 10},
+        {boss_id: 5, damage_id: 11},
+
+        // Deacons
+
+        // Wolnir
+        {boss_id: 7, damage_id: 9},
+        {boss_id: 7, damage_id: 10},
+        {boss_id: 7, damage_id: 11},
+        {boss_id: 7, damage_id: 12},
+
+        // Demon King
+        {boss_id: 8, damage_id: 9},
+        {boss_id: 8, damage_id: 12},
+
+        // Pontiff
+
+        // Yhorm
+        {boss_id: 10, damage_id: 6},
+        {boss_id: 10, damage_id: 9},
+        {boss_id: 10, damage_id: 10},
+        {boss_id: 10, damage_id: 11},
+        {boss_id: 10, damage_id: 12},
+
+        // Aldrich
+        {boss_id: 11, damage_id: 10},
+        {boss_id: 11, damage_id: 11},
+
+        // Dancer
+        {boss_id: 12, damage_id: 10},
+        {boss_id: 12, damage_id: 11},
+        {boss_id: 12, damage_id: 12},
+
+        // Dragonslayer Armor
+        {boss_id: 13, damage_id: 9},
+        {boss_id: 13, damage_id: 10},
+        {boss_id: 13, damage_id: 11},
+
+        // Oceiros
+        {boss_id: 14, damage_id: 10},
+        {boss_id: 14, damage_id: 11},
+
+        // Champion Gundyr
+
+        // Lorian
+        {boss_id: 16, damage_id: 10},
+        {boss_id: 16, damage_id: 11},
+
+        // Lothric
+        {boss_id: 17, damage_id: 10},
+        {boss_id: 17, damage_id: 11},
+
+        // Wyvern
+
+        // King of the Storm (dragon)
+
+        // Nameless King
+        {boss_id: 20, damage_id: 10},
+        {boss_id: 20, damage_id: 11},
+
+        // Soul of Cinder
+
+        // Gravetender
+
+        // Sister Friede
+        {boss_id: 23, damage_id: 12},
+
+        // Demon Prince
+        {boss_id: 24, damage_id: 10},
+        {boss_id: 24, damage_id: 11},
+        {boss_id: 24, damage_id: 12},
+
+        // Halflight
+
+        // Midir
+        {boss_id: 25, damage_id: 9},
+        {boss_id: 25, damage_id: 10},
+        {boss_id: 25, damage_id: 11},
+        {boss_id: 25, damage_id: 12},
+
+        // Gael
       ]);
     });
 };
