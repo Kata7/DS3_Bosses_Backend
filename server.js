23c127d4a2dc8ebb58a872bca13b5257919498ee
Austin Moran <a.moran3257@gmail.com>
2019-02-25 16:52:53 -0700

    Aimee helped with weaknesses subquery

--- server.js
+++ server.js
@@ -19,9 +19,34 @@ const table = 'Bosses'
 
 app.get('/', (req, res, next) => {
   knex('Bosses')
-  .select('boss_name', 'location_name')
+  .select('boss_name', 'boss_id', 'location_name')
   .join('Locations', 'Bosses.location_id', 'Locations.location_id')
-  .then((rows) => res.status(200).send(rows))
+  .then((bosses) => {
+    const bossesWithWeaknesses = bosses.map((boss) => {
+      const damageArray = []
+      return knex('Weaknesses')
+      .where({boss_id: boss.boss_id})
+      .select('damage_id')
+      .then(weaknesses => {
+        const getDamages = weaknesses.map((weakness) => {
+          return knex('DamageTypes')
+          .where({damage_id: weakness.damage_id})
+          .select('damage_name').first()
+          .then(damage => {
+            console.log('damages', damage)
+            damageArray.push(damage.damage_name)
+            boss.weaknesses = damageArray
+            return boss
+          }) 
+        })
+      return Promise.all(getDamages)
+      })
+    })
+    return Promise.all(bossesWithWeaknesses)
+  })
+  .then(rows => {
+    res.send(rows)
+  })
   .catch(err => {
     next(err)
   })
